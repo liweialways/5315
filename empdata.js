5877423cd360f4ab38f26102f87a34d8358d7c14
wei <liwei@seattleu.edu>
2019-04-22 15:26:07 -0700

    hw modify

--- empdata.js
+++ empdata.js
@@ -1,124 +1,109 @@
 {
   "data": [
     {
-      "name": "James Bond",
-      "designation": "Accountant",
-      "salary": "$170,750",
-      "joining_date": "2011/07/25",
-      "office": "Tokyo",
-      "extension": "8422"
+      "product": "Fred Meyer Large Eggs",
+      "type": "Dairy",
+      "price": "$2.65",
+      "expire_date": "2019/04/25",
+      "quantity": "100"
     },
     {
-      "name": "Brielle Williamson",
-      "designation": "Integration Specialist",
-      "salary": "$372,000",
-      "joining_date": "2012/12/02",
-      "office": "New York",
-      "extension": "4804"
+      "product": "Kroger Lactose Free Vitamin D Milk",
+      "type": "Dairy",
+      "price": "$3.49",
+      "expire_date": "2019/04/28",
+      "quantity": "100"
     },
     {
-      "name": "Ashton Cox",
-      "designation": "Junior Technical Author",
-      "salary": "$86,000",
-      "joining_date": "2009/01/12",
-      "office": "San Francisco",
-      "extension": "1562"
+      "product": "Kroger Lactose Free Calcium Enriched 2% Milk",
+      "type": "Dairy",
+      "price": "$2.65",
+      "expire_date": "2019/04/25",
+      "quantity": "100"
     },
     {
-      "name": "Airi Satou",
-      "designation": "Accountant",
-      "salary": "$162,700",
-      "joining_date": "2008/11/28",
-      "office": "Tokyo",
-      "extension": "5407"
+      "product": "Two Good By Light and Fit Mixed Berry Yogurt",
+      "type": "Dairy",
+      "price": "$1.49",
+      "expire_date": "2019/04/29",
+      "quantity": "200"
     },
     {
-      "name": "Caesar Vance",
-      "designation": "Pre-Sales Support",
-      "salary": "$106,450",
-      "joining_date": "2011/12/12",
-      "office": "New York",
-      "extension": "8330"
+     "product": "Kroger Dry Roasted Unsalted Peanuts",
+      "type": "Snacks",
+      "price": "$2.69",
+      "expire_date": "2020/01/21",
+      "quantity": "80"
     },
     {
-      "name": "Shad Decker",
-      "designation": "Regional Director",
-      "salary": "$183,000",
-      "joining_date": "2008/11/13",
-      "office": "Edinburgh",
-      "extension": "6373"
+      "product": "Kroger Lightly Salted Peanuts",
+      "type": "Snacks",
+      "price": "$1.99",
+      "expire_date": "2020/01/22",
+      "quantity": "100"
     },
     {
-      "name": "Cedric Kelly",
-      "designation": "Senior Javascript Developer",
-      "salary": "$433,060",
-      "joining_date": "2012/03/29",
-      "office": "Edinburgh",
-      "extension": "6224"
+      "product": "Nutter Butter Peanut Butter Cream Patties",
+      "type": "Snacks",
+      "price": "$3.49",
+      "expire_date": "2020/01/10",
+      "quantity": "90"
     },
     {
-      "name": "Haley Kennedy",
-      "designation": "Senior Marketing Designer",
-      "salary": "$313,500",
-      "joining_date": "2012/12/18",
-      "office": "London",
-      "extension": "3597"
+      "product": "Chex Mix Savory Bold Party Blend",
+      "type": "Snacks",
+      "price": "$2.89",
+      "expire_date": "2019/01/11",
+      "quantity": "100"
     },
     {
-      "name": "Colleen Hurst",
-      "designation": "Javascript Developer",
-      "salary": "$205,500",
-      "joining_date": "2009/09/15",
-      "office": "San Francisco",
-      "extension": "2360"
+      "product": "Kroger Animal Crackers",
+      "type": "Snacks",
+      "price": "$1.99",
+      "expire_date": "2020/01/01",
+      "quantity": "100"
     },
     {
-      "name": "Dai Rios",
-      "designation": "Personnel Lead",
-      "salary": "$217,500",
-      "joining_date": "2012/09/26",
-      "office": "Edinburgh",
-      "extension": "2290"
+      "product": "White Seedless Grapes",
+      "type": "Produce",
+      "price": "$2.99",
+      "expire_date": "2019/04/28",
+      "quantity": "50"
     },
     {
-      "name": "Herrod Chandler",
-      "designation": "Sales Assistant",
-      "salary": "$137,500",
-      "joining_date": "2012/08/06",
-      "office": "San Francisco",
-      "extension": "9608"
+      "product": "Banana",
+      "type": "Produce",
+      "price": "$0.3",
+      "expire_date": "2019/04/28",
+      "quantity": "50"
     },
     {
-      "name": "Rhona Davidson",
-      "designation": "Integration Specialist",
-      "salary": "$327,900",
-      "joining_date": "2010/10/14",
-      "office": "Tokyo",
-      "extension": "6200"
+      "product": "Onions Green",
+      "type": "Produce",
+      "price": "$0.59",
+      "expire_date": "2019/04/28",
+      "quantity": "50"
     },
     {
-      "name": "Sonya Frost",
-      "designation": "Software Engineer",
-      "salary": "$103,600",
-      "joining_date": "2008/12/13",
-      "office": "Edinburgh",
-      "extension": "1667"
+      "product": "Tomatoes",
+      "type": "Produce",
+      "price": "$0.5",
+      "expire_date": "2019/04/28",
+      "quantity": "50"
     },
     {
-      "name": "Jena Gaines",
-      "designation": "Office Manager",
-      "salary": "$90,560",
-      "joining_date": "2008/12/19",
-      "office": "London",
-      "extension": "3814"
+      "product": "Lettuce",
+      "type": "Produce",
+      "price": "$0.4",
+      "expire_date": "2019/04/28",
+      "quantity": "50"
     },
     {
-      "name": "Quinn Flynn",
-      "designation": "Support Lead",
-      "salary": "$342,000",
-      "joining_date": "2013/03/03",
-      "office": "Edinburgh",
-      "extension": "9497"
+      "product": "Avocado",
+      "type": "Produce",
+      "price": "$2.49",
+      "expire_date": "2019/04/28",
+      "quantity": "50"
     }
   ]
 }
